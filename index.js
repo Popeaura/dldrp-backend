faafa4ff60c330b32c385191a0eef97ea1f81c14
popeaura <popeahura@gmail.com>
2025-05-08 12:38:00 +0300

    I uploaded my modifed project's folder

---
+++ index.js
@@ -0,0 +1,16 @@
+import { create } from 'helia'
+
+async function main() {
+  // Initialize Helia
+  const helia = await create()
+
+  // Add a file to IPFS
+  const { cid } = await helia.add('Hello, IPFS with Helia!')
+  console.log('File added with CID:', cid.toString())
+
+  // Retrieve the file from IPFS
+  const content = await helia.cat(cid)
+  console.log('Content from IPFS:', content.toString())
+}
+
+main().catch(console.error)
